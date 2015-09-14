a97413a3d0feffa052fea575e33e939d7d49fb4b
martin <martin.hinz@ufg.uni-kiel.de>
2015-09-14 18:08:10 +0200

    Finally in working state?

--- R/roxcalCalibratedDatesList.R
+++ R/roxcalCalibratedDatesList.R
@@ -19,8 +19,6 @@ plot.roxcalCalibratedDatesList <- function(x, ...){
   }
 }
 
-
-
 plotRoxcalCalibratedDatesListSystemGraphics<-function(x, ...){
   op <- par(no.readonly = TRUE)
   indices <- 1:length(x)
