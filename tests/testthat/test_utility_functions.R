ad8975bfc74360cd9b97cb5240081592e891444b
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 16:34:45 +0200

    added test for catch file not existing in setOxcalExecutablePath

--- tests/testthat/test_utility_functions.R
+++ tests/testthat/test_utility_functions.R
@@ -5,6 +5,10 @@ test_that("setOxcalExecutablePath sets the oxcal path in the environment variabl
   expect_true(options("oxcAAR.oxcal_path")=="ox_output.js")
 })
 
+test_that("setOxcalExecutablePath complains when file does not exists",{
+  expect_error(setOxcalExecutablePath("i_am_not_here.file"), "No file at given location")
+})
+
 context("quickSetupOxcal")
 test_that("quickSetupOxcal downloads oxcal and sets correct path",{
   expect_error(quickSetupOxcal(), NA)
