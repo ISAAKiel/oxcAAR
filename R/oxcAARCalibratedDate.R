e4af8749a9e43f312384c41c16667145e5e9e493
nevrome <clemens@nevrome.de>
2017-07-21 16:07:04 +0200

    added getter functions for oxcAARCalibratedDate - no tests for that yet

--- R/oxcAARCalibratedDate.R
+++ R/oxcAARCalibratedDate.R
@@ -110,3 +110,118 @@ plotoxcAARDateSystemGraphics <- function(x, ...){
   graphics::mtext(paste(x$cal_curve), side = 1, line = 4, adj = 0,
         cex = 0.6)
 }
+
+#### getter ####
+
+# helper function to check class attribute
+check_if_class_is_oxcAARCalibratedDate <- function(x) {
+  if (!("oxcAARCalibratedDate" %in% class(x))) stop("x is not an object of class oxcAARCalibratedDate")
+}
+
+#' get date name
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return a string
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_name(x)
+#' }
+#'
+get_name <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["name"]])
+}
+
+#' get date bp value (age)
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return an integer
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_bp(x)
+#' }
+#'
+get_bp <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["bp"]])
+}
+
+#' get date std value (standard deviation)
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return an integer
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_std(x)
+#' }
+#'
+get_std <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["std"]])
+}
+
+#' get calibration curve name
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return a string
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_cal_curve(x)
+#' }
+#'
+get_cal_curve <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["cal_curve"]])
+}
+
+#' get sigma ranges
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return a list of three data.frames
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_sigma_ranges(x)
+#' }
+#'
+get_sigma_ranges <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["sigma_ranges"]])
+}
+
+#' get raw probabilities
+#'
+#' @param x an object of class oxcAARCalibratedDate
+#'
+#' @return a data.frame
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' x <- oxcalCalibrate(5000, 20)[[1]]
+#' get_raw_probabilities(x)
+#' }
+#'
+get_raw_probabilities <- function(x) {
+  check_if_class_is_oxcAARCalibratedDate(x)
+  return(x[["raw_probabilities"]])
+}
