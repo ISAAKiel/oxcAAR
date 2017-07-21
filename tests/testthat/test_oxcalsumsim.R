f00d4b11846c70dd53f5f57c957502862bbdb3f9
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 15:09:14 +0200

    fixed availability of ox_sum.js for all tests in test_oxcalSumSim

--- tests/testthat/test_oxcalsumsim.R
+++ tests/testthat/test_oxcalsumsim.R
@@ -19,14 +19,17 @@ with_mock(
       expect_error(suppressWarnings(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10)), NA)
     })
 
+    file.copy("ox_sum.js", "ox_sum_work.js")
     test_that("oxcalSumSim is processed without error given oxcal result file with uniform distribution", {
       expect_error(suppressWarnings(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10,date_distribution = "uniform")), NA)
     })
 
+    file.copy("ox_sum.js", "ox_sum_work.js")
     test_that("oxcalSumSim complains when given wrong amount of stds", {
       expect_error(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = c(10,10)), "Please give either one stds")
     })
 
+    file.copy("ox_sum.js", "ox_sum_work.js")
     test_that("oxcalSumSim complains when given wrong distribution", {
       expect_error(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10, date_distribution = "kaffeemühle" ), "should be one of")
     })
