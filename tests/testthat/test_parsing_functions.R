7124033f8e45d521eaa8a49a8bf7bda7d56f22af
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 14:11:27 +0200

    silly typo on test_parsing_functions

--- tests/testthat/test_parsing_functions.R
+++ tests/testthat/test_parsing_functions.R
@@ -1,19 +1,19 @@
 context("R_Date")
 
-testthat("R_Date produces correct oxcal code", {
+test_that("R_Date produces correct oxcal code", {
   expect_equal(R_Date("date", 4000, 10), "R_Date(\"date\", 4000, 10);")
 }
          )
 
 context("R_Simulate")
 
-testthat("R_Simulate produces correct oxcal code", {
+test_that("R_Simulate produces correct oxcal code", {
   expect_equal(R_Simulate(4000, 10), "R_Simulate(\"1\",\n          4000, 10);")
 })
 
 
 context("oxcal_Sum")
 
-testthat("oxcal_Sum produces correct oxcal code", {
+test_that("oxcal_Sum produces correct oxcal code", {
   expect_equal(oxcal_Sum(R_Simulate(4000, 10)), "Sum(\" Sum \"){\n R_Simulate(\"1\",\n          4000, 10); \n};")
 })
