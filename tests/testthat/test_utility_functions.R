10c3dda9ec1eecdf599e871439c24b5c9100d6f8
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 16:08:52 +0200

    added test for quickSetupOxcal

--- tests/testthat/test_utility_functions.R
+++ tests/testthat/test_utility_functions.R
@@ -4,3 +4,12 @@ test_that("setOxcalExecutablePath sets the oxcal path in the environment variabl
   setOxcalExecutablePath("ox_output.js")
   expect_true(options("oxcAAR.oxcal_path")=="ox_output.js")
 })
+
+context("quickSetupOxcal")
+test_that("quickSetupOxcal downloads oxcal and sets correct path",{
+  expect_error(quickSetupOxcal(), NA)
+  expect_true(dir.exists("OxCal/bin/"))
+  expect_true(basename(options("oxcAAR.oxcal_path")[[1]]) %in% c("OxCalLinux",
+                                                                 "OxCalWin.exe",
+                                                                 "OxCalMac"))
+})
