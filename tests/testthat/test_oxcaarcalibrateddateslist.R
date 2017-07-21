05d40bdfc7f5b83198a7ace8edeb2436841581b3
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 13:09:32 +0200

    corrected test for oxcAARCalibratedDatesList functions to reflect the list character correctly

--- tests/testthat/test_oxcaarcalibrateddateslist.R
+++ tests/testthat/test_oxcaarcalibrateddateslist.R
@@ -1,5 +1,6 @@
 result <- readOxcalOutput("ox_output.js")
 this_date_list <- parseOxcalOutput(result)
+this_date_list[[1]]$name <- "date"
 
 context("print.oxcAARCalibratedDatesList")
 
@@ -11,6 +12,10 @@ test_that("print is delegated to print.oxcAARCalibratedDatesList on a oxcAARCali
 
 context("plot.oxcAARCalibratedDatesList")
 
+this_multiple_dates_list <- this_date_list
+this_multiple_dates_list[[2]] <- this_date_list[[1]]
+names(this_multiple_dates_list) <- c("date1", "date2")
+
 test_that("plot produces no error", {
-  expect_error(plot( this_date_list), NA)
+  expect_error(plot( this_multiple_dates_list), NA)
 })
