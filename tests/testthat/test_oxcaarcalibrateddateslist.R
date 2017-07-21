97666ac11af9e0cd3b507cee6b99f9a56e740492
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 13:20:35 +0200

    added test for print multiple dates from print.oxcAARCalibratedDatesList

--- tests/testthat/test_oxcaarcalibrateddateslist.R
+++ tests/testthat/test_oxcaarcalibrateddateslist.R
@@ -2,19 +2,25 @@ result <- readOxcalOutput("ox_output.js")
 this_date_list <- parseOxcalOutput(result)
 this_date_list[[1]]$name <- "date"
 
+this_multiple_dates_list <- this_date_list
+this_multiple_dates_list[[2]] <- this_date_list[[1]]
+names(this_multiple_dates_list) <- c("date1", "date2")
+
 context("print.oxcAARCalibratedDatesList")
 
-test_that("print is delegated to print.oxcAARCalibratedDatesList on a oxcAARCalibratedDatesList", {
+test_that("print is delegated to print.oxcAARCalibratedDate on a oxcAARCalibratedDatesList with just one entry", {
   expect_output(print(this_date_list), "BP")
+  expect_failure(expect_output(print(this_date_list), "produced output"))
 })
+
+test_that("print is delegated to print.oxcAARCalibratedDatesList on a oxcAARCalibratedDatesList with multiple entries", {
+  expect_output(print(this_multiple_dates_list), "List of 2")
+})
+
 
 # Dummy test for plots
 
 context("plot.oxcAARCalibratedDatesList")
-
-this_multiple_dates_list <- this_date_list
-this_multiple_dates_list[[2]] <- this_date_list[[1]]
-names(this_multiple_dates_list) <- c("date1", "date2")
 
 test_that("plot produces no error", {
   expect_error(plot( this_multiple_dates_list), NA)
