41b7e2df9f9aa5eb5d7d10f3033dcf8f4c9f100b
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 11:00:09 +0200

    added example oxcal output and first useful test

--- tests/testthat/test_calibrate.R
+++ tests/testthat/test_calibrate.R
@@ -1,7 +1,13 @@
 with_mock(
   `executeOxcalScript`= function(...) {
-    return("ox_output.js");
+    return("ox_output_wrong.js");
   }, {
     expect_error( suppressWarnings(oxcalCalibrate(5000,25,"KIA-12345")))
   })
 
+with_mock(
+  `executeOxcalScript`= function(...) {
+    return("ox_output.js");
+  }, {
+    expect_error( suppressWarnings(oxcalCalibrate(5000,25,"KIA-12345")), NA)
+  })
