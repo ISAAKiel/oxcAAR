9add2ac16a6e4e81514b4d2ddafd95dafc5b72b0
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2017-07-21 14:53:34 +0200

    added test for oxcalSumSim

---
+++ tests/testthat/test_oxcalsumsim.R
@@ -0,0 +1,26 @@
+context("oxcalSumSim")
+
+test_that("oxcalSumSim produces error given wrong oxcal result file", {
+  with_mock(
+    `executeOxcalScript`= function(...) {
+      return("ox_output_wrong.js");
+    }, {
+      expect_error( suppressWarnings(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10)))
+    })
+})
+
+with_mock(
+  `executeOxcalScript`= function(...) {
+    return("ox_sum_work.js");
+  }, {
+    file.copy("ox_sum.js", "ox_sum_work.js")
+    test_that("oxcalSumSim is processed without error given oxcal result file", {
+      expect_error(suppressWarnings(oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10)), NA)
+    })
+    file.copy("ox_sum.js", "ox_sum_work.js")
+    this_result <- oxcalSumSim(timeframe_begin = -1000, timeframe_end = -2000, n = 10, stds = 10)
+
+    test_that("oxcalSimulate is produces an oxcAARCalibratedDate", {
+      expect_equal(class(this_result), "oxcAARCalibratedDate")
+    })
+  })
