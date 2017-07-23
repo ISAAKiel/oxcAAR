140a3dde01c46b4723f2ff4831107a3e2f51544c
nevrome <clemens@nevrome.de>
2017-07-23 22:23:09 +0200

    revised and added continuity tests for getter functions

--- tests/testthat/test_oxcaarcalibrateddate.R
+++ tests/testthat/test_oxcaarcalibrateddate.R
@@ -39,39 +39,3 @@ test_that("is.oxcAARCalibratedDate distinguishes between objects", {
   expect_true(is.oxcAARCalibratedDate(this_date))
   expect_false(is.oxcAARCalibratedDate(data.frame(test=NA)))
 })
-
-context("get_name")
-
-test_that("get_name returns name", {
-  expect_equal(get_name(this_date), this_date[['name']])
-})
-
-context("get_bp")
-
-test_that("get_bp returns BP", {
-  expect_equal(get_bp(this_date), this_date[['bp']])
-})
-
-context("get_std")
-
-test_that("get_std returns std", {
-  expect_equal(get_std(this_date), this_date[['std']])
-})
-
-context("get_cal_curve")
-
-test_that("get_cal_curve returns std", {
-  expect_equal(get_cal_curve(this_date), this_date[['cal_curve']])
-})
-
-context("get_sigma_ranges")
-
-test_that("get_sigma_ranges returns sigma_ranges", {
-  expect_equal(get_sigma_ranges(this_date), this_date[['sigma_ranges']])
-})
-
-context("get_sigma_ranges")
-
-test_that("get_raw_probabilities returns raw_probabilities", {
-  expect_equal(get_raw_probabilities(this_date), this_date[['raw_probabilities']])
-})
